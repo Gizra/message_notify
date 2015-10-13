bca69cb581e73806adbb0506ff7acc82f1c02a63
Kirill Roskolii <kirill.roskolii@catalyst.net.nz>
2015-10-13 17:05:31 +1300

    Issue #2386273 by alfaguru, Andy_D, RoSk0: Decode HTML entities in subject.

--- plugins/notifier/email/MessageNotifierEmail.class.php
+++ plugins/notifier/email/MessageNotifierEmail.class.php
@@ -1,10 +1,18 @@
 <?php
 
 /**
+ * @file
+ * Email notifier plugin class.
+ */
+
+/**
  * Email notifier.
  */
 class MessageNotifierEmail extends MessageNotifierBase {
 
+  /**
+   * Implements MessageNotifierInterface::deliver().
+   */
   public function deliver(array $output = array()) {
     $plugin = $this->plugin;
     $message = $this->message;
@@ -16,19 +24,23 @@ class MessageNotifierEmail extends MessageNotifierBase {
 
     $languages = language_list();
     if (!$options['language override']) {
-      $lang = !empty($account->language) && $account->language != LANGUAGE_NONE ? $languages[$account->language]: language_default();
+      $lang = !empty($account->language) && $account->language != LANGUAGE_NONE ? $languages[$account->language] : language_default();
     }
     else {
       $lang = $languages[$message->language];
     }
-
+    // Start with basic clean-up to remove line-feeds etc.
+    $output['message_notify_email_subject'] = trim($output['message_notify_email_subject']);
     // The subject in an email can't be with HTML, so strip it.
     $output['message_notify_email_subject'] = strip_tags($output['message_notify_email_subject']);
+    // Decode entities.
+    $output['message_notify_email_subject'] = decode_entities($output['message_notify_email_subject']);
 
     // Pass the message entity along to hook_drupal_mail().
     $output['message_entity'] = $message;
 
-    $result =  drupal_mail('message_notify', $message->type, $mail, $lang, $output);
+    $result = drupal_mail('message_notify', $message->type, $mail, $lang, $output);
+
     return $result['result'];
   }
 
