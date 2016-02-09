81c346fa4022a7cc5cef24040217067e13482a36
Jonathan Hedstrom <jhedstrom@gmail.com>
2016-02-09 14:59:56 -0800

    Adding the missing plugin annotation

---
+++ src/Annotation/Notifier.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\message_notify\Annotation\Notifier.
+ */
+
+namespace Drupal\message_notify\Annotation;
+
+use Drupal\Component\Annotation\Plugin;
+
+/**
+ * Defines a notifier plugin.
+ */
+class Notifier extends Plugin {
+
+  /**
+   * The plugin ID.
+   *
+   * @var string
+   */
+  public $id;
+
+  /**
+   * The human-readable title.
+   *
+   * @ingroup plugin_translatable
+   *
+   * @var \Drupal\Core\Annotation\Translation
+   */
+  public $title;
+
+}
