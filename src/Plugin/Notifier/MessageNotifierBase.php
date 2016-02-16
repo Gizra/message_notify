dfcf5ca00645b69633eb0d4e2eb4155e8d579ca9
Jonathan Hedstrom <jhedstrom@gmail.com>
2016-02-16 12:30:12 -0800

    Fixes tests

--- src/Plugin/Notifier/MessageNotifierBase.php
+++ src/Plugin/Notifier/MessageNotifierBase.php
@@ -107,28 +107,27 @@ abstract class MessageNotifierBase extends PluginBase implements MessageNotifier
       $save = TRUE;
     }
 
-    // @todo Port this bit to 8.
     if (isset($this->configuration['rendered fields'])) {
-      // @todo View mode support.
-      foreach ($this->configuration['view_modes'] as $view_mode => $mode) {
-        if (empty($options['rendered fields'][$view_mode])) {
+      foreach ($this->pluginDefinition['view_modes'] as $view_mode) {
+        if (empty($this->configuration['rendered fields'][$view_mode])) {
           throw new MessageNotifyException('The rendered view mode "' . $view_mode . '" cannot be saved to field, as there is not a matching one.');
         }
-        $field_name = $options['rendered fields'][$view_mode];
+        $field_name = $this->configuration['rendered fields'][$view_mode];
 
         // @todo Inject the content_type.manager if this check is needed.
-        if (!$field = field_info_field($field_name)) {
+        if (!$field = $this->entityTypeManager->getStorage('field_config')->load('message.' . $this->message->bundle() . '.' . $field_name)) {
           throw new MessageNotifyException('Field "' . $field_name . '"" does not exist.');
         }
 
         // Get the format from the field. We assume the first delta is the
         // same as the rest.
-        if (empty($wrapper->{$field_name}->format)) {
-          $wrapper->{$field_name}->set($output[$view_mode]);
+        if (!$format = $this->message->get($field_name)->format) {
+          // Field has no formatting.
+          // @todo Centralize/unify rendering.
+          $this->message->set($field_name, $output[$view_mode]['#markup']);
         }
         else {
-          $format = $wrapper->type->{MESSAGE_FIELD_MESSAGE_TEXT}->get(0)->format->value();
-          $wrapper->{$field_name}->set(['value' => $output[$view_mode], 'format' => $format]);
+          $this->message->set($field_name, ['value' => $output[$view_mode], 'format' => $format]);
         }
       }
     }
